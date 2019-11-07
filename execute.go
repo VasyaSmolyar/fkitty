4f5c87f619bec0d99d2a5525cbb8cb652026f4c2
VasyaSmolyar <smolyar4@gmail.com>
2019-11-07 22:27:38 +0300

    Improve a authentication

--- execute.go
+++ execute.go
@@ -26,21 +26,40 @@ func execute(tokens []string, ftp *FtpConnect, ans *FtpAnswer) {
 	}
 }
 
+func getUserData(user string) map[string]string {
+	for _, val := range users {
+		if val["login"] == user {
+			return val
+		}
+	}
+	return nil
+}
+
 func user(args []string, ftp *FtpConnect, ans *FtpAnswer) {
+	if ftp.logged == true {
+		ans.code = 530 
+		ans.status = "You're already logged in"
+		return
+	}
 	ftp.user = args[0]
-	ftp.logged = false
 	ans.code = 331
 	ans.status = "User " + ftp.user + " OK. Password required"
 }
 
 func pass(args []string, ftp *FtpConnect, ans *FtpAnswer) {
+	if ftp.logged == true {
+		ans.code = 530 
+		ans.status = "We can't do that in the current session"
+		return
+	}
 	hash := sha512.New();
 	hash.Write([]byte(args[0]))
 	if hex.EncodeToString(hash.Sum(nil)) == passes[ftp.user] {
+		data := getUserData(ftp.user)
 		ftp.logged = true
+		ftp.dir = data["dir"]
 		ans.code = 230
-		// TODO: add a getting directory name
-		ans.status = "OK. Current directory is"
+		ans.status = "OK. Current directory is " + data["dir"]
 	} else {
 		ans.code = 530
 		ans.status = "Login authentication failed"
