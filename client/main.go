4f496de005d67eac4cba454fd3a5e7ac484a41b0
VasyaSmolyar <smolyar4@gmail.com>
2019-11-10 00:26:41 +0300

    Add a base functional in the client

--- client/main.go
+++ client/main.go
@@ -6,7 +6,14 @@ import (
 	"os"
 	"flag"
 	"bufio"
+	"strings"
 )
+
+func alloc() map[string]func (com string) bool {
+	return map[string]func (com string) bool {
+		"QUIT" : quit,
+	}
+}
 
 func send(conn net.Conn, data []byte) string {
 	if data != nil {
@@ -18,7 +25,7 @@ func send(conn net.Conn, data []byte) string {
 	return string(res)
 }
 
-func login(conn net.Conn, name string) {
+func login(conn net.Conn, name string) bool {
 	if name == "" {
 		fmt.Print("login as: ")
 		fmt.Scanln(&name)
@@ -27,23 +34,50 @@ func login(conn net.Conn, name string) {
 	var pass string
 	fmt.Print("Password: ")
 	fmt.Scanln(&pass)
-	fmt.Print(send(conn, []byte("PASS " + pass)))
+	res := send(conn, []byte("PASS " + pass))
+	fmt.Print(res)
+	return strings.HasPrefix(res, "230")
 }
 
 func main() {
 	args := os.Args
-	name := flag.String("u", "", "Username")
+	var name string 
+	flag.StringVar(&name ,"u", "", "Username")
 	if len(args) == 1 {
 		fmt.Println("Fkitty Client: You must specify a host:port to connect to.")
 		return
 	}
 	flag.Parse() 
-	host := os.Args[1]
+	host := args[len(args)-1]
 	conn, err := net.Dial("tcp", host)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Print(send(conn, nil))
-	login(conn, *name)
+	log := login(conn, name)
+	for !log {
+		log = login(conn, "")
+	}
+	var com string
+	work := true
+	methods := alloc()
+	for work {
+		fmt.Print("fkitty: ")
+		l, _ := fmt.Scanln(&com)
+		if l == 0 {
+			continue
+		}
+		res := send(conn, []byte(com))
+		fmt.Print(res)
+		for k,v := range methods {
+			if strings.HasPrefix(strings.ToUpper(com), k) {
+				work = v(com)
+			}
+		}
+	}
+}
+
+func quit(com string) bool {
+	return false;
 }
